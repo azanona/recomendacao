d9f408a81d0bf131bd5b587230f7cae6199cdba5
Arthur Zanona <arthurzanona@gmail.com>
2017-02-13 22:34:41 -0200

    Descoberta de serviços cliente/servidor; Api de gateway - Zuul

---
+++ server/descoberta-servico/src/main/java/br/com/zanona/tcc/DescobertaServicoApp.java
@@ -0,0 +1,14 @@
+package br.com.zanona.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DescobertaServicoApp {
+
+	    public static void main(String[] args) {
+	        SpringApplication.run(DescobertaServicoApp.class, args);
+	    }
+}
--- server/turismo/src/main/java/br/com/zanona/turismo/App.java
+++ server/turismo/src/main/java/br/com/zanona/turismo/App.java
@@ -1,10 +1,10 @@
 package br.com.zanona.turismo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
 
